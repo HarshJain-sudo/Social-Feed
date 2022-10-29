efb6b91bdceffcabd4a8467c05b6d644f428d02a
hj60490 <63598793+hj60490@users.noreply.github.com>
2022-10-29 12:24:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,11 +14,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://harsh:harsh@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb+srv://rahul:rahul@cluster0.nhoty77.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
